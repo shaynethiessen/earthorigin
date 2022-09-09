78d70dccd924bfd2d9c3c16a2cf0ed5b6665b2d7
Shayne Thiessen <shayne@shaynethiessen.com>
2022-09-09 10:50:17 -0500

    feat: create server

--- packages/web/src/index.tsx
+++ packages/web/src/index.tsx
@@ -1,7 +1,7 @@
 import debug from 'debug';
 import type React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './core/App';
 
 const d = debug('earthorigin.web');
 
