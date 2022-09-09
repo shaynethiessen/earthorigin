86ef3ea96ea108ef4be1d717aaf7bcd5748a92ad
Shayne Thiessen <shayne@shaynethiessen.com>
2022-09-09 11:17:24 -0500

    feat: add missing deps

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,5 +1,15 @@
 import debug from "debug";
+import {mikroOrmConfig} from "./core/mikro-orm.config";
+import {MikroORM} from "@mikro-orm/core";
+import cors from 'cors';
+import Express from 'express';
 
 const d = debug('earthorigin.server');
 
 d('Start Server');
+
+MikroORM.init(mikroOrmConfig()).then(orm => {
+    const express = Express();
+    express.use(cors());
+    express.use(Express.json());
+});
