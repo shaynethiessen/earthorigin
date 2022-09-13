0043a838b43cfc825f50712494f49da73010a882
Shayne Thiessen <shayne@shaynethiessen.com>
2022-09-13 07:44:23 -0500

    feat: get domain from environment

--- packages/web/src/pages/Home/Content.tsx
+++ packages/web/src/pages/Home/Content.tsx
@@ -1,14 +1,15 @@
 import React, {useState} from 'react';
 import ReactPlayer from 'react-player/file';
+import {environment} from "../../core/environment";
 
 export function Content() {
 	const [count, setCount] = useState(0);
 	const [playing, setPlaying] = useState(true);
 	const videos = [
-		'http://localhost:3001/2022/09/12/1.mp4',
-		'http://localhost:3001/2022/09/12/2.mp4',
-		'http://localhost:3001/2022/09/12/3.mp4',
-		'http://localhost:3001/2022/09/12/4.mp4',
+		'/2022/09/12/1.mp4',
+		'/2022/09/12/2.mp4',
+		'/2022/09/12/3.mp4',
+		'/2022/09/12/4.mp4',
 	];
 
 	function nextVideo() {
@@ -22,7 +23,7 @@ export function Content() {
 
 	return (
 		<div style={{width: '100%', height: '100%', padding: 0, margin: 0}}>
-			<ReactPlayer url={videos[count]} playing={playing} onEnded={nextVideo} controls={true} width={'95%'} height={'100%'} style={{}} />
+			<ReactPlayer url={`${environment.serverURL}${videos[count]}`} playing={playing} onEnded={nextVideo} controls={true} width={'95%'} height={'100%'} style={{}} />
 		</div>
 	);
 }
